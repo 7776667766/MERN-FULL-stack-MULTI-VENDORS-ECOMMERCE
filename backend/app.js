255fef9c74a3eb93c7f499a75259843820191cb2
ahmad mujtaba <74832715+7776667766@users.noreply.github.com>
2023-08-04 06:02:17 -0700

    deployment

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,8 @@ const cors = require("cors");
 require('dotenv').config();
 const path =require("path")
 
-app.use(cors({
-  origin: ['https://mern-full-stack-multi-vendors-ecommerce.vercel.app',],
-  credentials: true
-}));
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
